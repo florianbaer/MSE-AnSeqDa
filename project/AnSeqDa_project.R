bb3a53eafdf5cc54b68cb70a60a0e074131b9ea8
Florian Baer <florianbaer@bluewin.ch>
2022-11-22 15:57:09 +0100

    Work on project

--- project/AnSeqDa_project.R
+++ project/AnSeqDa_project.R
@@ -1,3 +1,19 @@
 library(Mcomp)
+library(ggplot2)
 all_data <- subset(M3, 'quarterly')[[300]]
-autoplot(all_data)
+all_data
+train <- all_data$x
+test <- all_data$xx
+
+str(all_data)
+
+# autoplot
+autoplot(all_data) + ylab('Volume Indeces') + xlab('Year') + ggtitle('SWITZERLAND Gross Domestic Product')
+
+
+# seasonal plot
+ggseasonplot(train, year.labels=TRUE, year.labels.left=TRUE) +
+  ylab("Volume index") +
+  ggtitle("Seasonal plot: SWITZERLAND Gross Domestic Product")
+
+# seasonal subseries plot
